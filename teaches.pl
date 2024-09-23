d12a883b5f62305be0363788a8fcb6e69862ff31
snehalatha-1692 <141340049+snehalatha-1692@users.noreply.github.com>
2024-09-23 09:37:35 +0530

    Create teaches.pl

---
+++ teaches.pl
@@ -0,0 +1,8 @@
+teaches(lisa,prof_lara,math,1234).
+teaches(sam,prof_bot,chemistry,234).
+enrolled(sai,ai,1222).
+enrolled(veera,ai,1222).
+db_students(Name,teacher,subject,code):-
+    teaches(Name,teacher,subject,code).
+db_enrolled(name,subject,code):-
+    enrolled(name,subject,code
